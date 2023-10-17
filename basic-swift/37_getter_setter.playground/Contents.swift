ad8504d97515f3b0c703089533c7bc496701c43d
최민재 <cmjcmj1230@gmail.com>
2023-10-17 16:37:09 +0900

    study getter setter

---
+++ basic-swift/37_getter_setter.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class Friend {
+    
+    var name: String
+    var age: Int
+    
+    var detailInfo : String = ""
+    
+    var info : String {
+        get {
+            return "내 친구: \(name) / 나이 : \(age)"
+        }
+        set {
+            detailInfo = "info에서 설정됨 :" + newValue
+        }
+    }
+    
+    init(_ name: String, _ age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
+let myFriend = Friend("쩡대리", 20)
+
+myFriend.info = "호로롤롤"
+myFriend.detailInfo
