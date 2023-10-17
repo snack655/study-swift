263391b407b1414dfe62957ea95d0cfebc6bfd0a
최민재 <cmjcmj1230@gmail.com>
2023-10-17 12:21:32 +0900

    study flatmap

---
+++ basic-swift/33_flatmap.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+let myFriends = [
+    ["철수"],
+    ["영희"],
+    ["제임스", "본드"],
+    ["스티브", "잡스"],
+    ["개발하는", "정대리"],
+]
+
+let flatMapped  = myFriends.flatMap { (item: [String]) in
+    return item
+}
+
+// 배열이 일차원으로 납작해짐.
+
+print(flatMapped)
