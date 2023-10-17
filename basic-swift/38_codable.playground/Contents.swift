634b6b7487f809713ecf1f639715950f30c2c683
최민재 <cmjcmj1230@gmail.com>
2023-10-17 16:53:58 +0900

    study multi trailing closer

---
+++ basic-swift/38_codable.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+let jsonFromServer = """
+{
+    "nick_name": "개발하는 정대리",
+    "job": "유튜바, 개발자",
+    "user_name": "dev_jeongdaeri",
+}
+"""
+
+// de code = Json을 클래스, 스트럭트로 바꾸기
+// en code = 제이슨으로 만들기
+
+// Encodable & Decodeable = Codable
+
+struct User : Decodable {
+    
+    var nickname: String
+    var job: String
+    var myUserName: String
+    
+    enum CodingKeys: String, CodingKey {
+        case nickname = "nick_name"
+        case job
+        case myUserName = "user_name"
+    }
+    
+    static func getUserFromJson(_ jsonString: String) -> Self? {
+        guard let jsonData : Data = jsonFromServer.data(using: .utf8) else {
+            return nil
+        }
+        
+        do {
+            let user = try JSONDecoder().decode(User.self, from: jsonData)
+            print("user: \(user)")
+            return user
+        } catch {
+            print("에러발생: \(error.localizedDescription)")
+            return nil
+        }
+    }
+}
+
+let user = User.getUserFromJson(jsonFromServer)
+
