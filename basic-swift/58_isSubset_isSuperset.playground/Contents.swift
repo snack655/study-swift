30382ac897a2e88df936cbd8e4b1f9b853b9e696
최민재 <cmjcmj1230@gmail.com>
2023-10-19 11:57:10 +0900

    study isSubset & isSuperset

---
+++ basic-swift/58_isSubset_isSuperset.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+let totalFriends = ["철수", "영희", "수잔", "제임스"]
+let foreignFriends = ["수잔", "제임스"]
+let koreanFriends = ["철수", "영희", "상철"]
+
+let totalFriendsSet = Set(totalFriends)
+let foreignFriendsSet = Set(foreignFriends)
+let koreanFriendsSet = Set(koreanFriends)
+
+// isSubset 내가 속해 있는지 체크
+let isForeignFriendsIsInTotalFriends = foreignFriendsSet.isSubset(of: totalFriendsSet)
+// true
+
+let isKoreanFriendsIsInTotalFriends = koreanFriendsSet.isSubset(of: totalFriendsSet)
+// false
+
+// isSuperset 내가 포함하고 있는지 체크
+let isTotalFriendsHasForeignFriends = totalFriendsSet.isSuperset(of: foreignFriends)
+// true
+
+let isTotalFriendsHasKoreanFriends = totalFriendsSet.isSuperset(of: koreanFriendsSet)
+// false
+
