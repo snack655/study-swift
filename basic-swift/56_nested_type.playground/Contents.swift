cdd0f4090d501b49c7ef10dccfb0aaddc8889bbc
최민재 <cmjcmj1230@gmail.com>
2023-10-19 10:34:45 +0900

    study nested type

---
+++ basic-swift/56_nested_type.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+struct MyPet {
+    enum Kind {
+        case cat
+        case dog
+        var value: String {
+            switch self {
+            case .cat:
+                return "고양이"
+            case .dog:
+                return "강아지"
+            }
+        }
+        
+        var name: String {
+            switch self {
+            case .cat:
+                return "개냥이"
+            case .dog:
+                return "해피"
+            }
+        }
+    } // Kind enum
+    
+    let kind: Kind
+    var description: String {
+        return "우리지 \(kind.value) \(kind.name)"
+    }
+}
+
+let myCat = MyPet(kind: .cat)
+print(myCat.description)
+
+if myCat.kind == MyPet.Kind.cat {
+    print("고양이가 입니다.")
+}
+
+let myDog = MyPet(kind: .dog)
+print(myDog.description)
