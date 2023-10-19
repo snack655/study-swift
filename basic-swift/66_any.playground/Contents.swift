681c5fa1e55c75550ba925f625444dc650ea1eff
최민재 <cmjcmj1230@gmail.com>
2023-10-19 14:58:26 +0900

    study any

---
+++ basic-swift/66_any.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class Animal {
+    
+}
+
+// any
+
+// Any를 통해 어떠한 자료형도 받을 수 있습니다.
+
+var someValue : Any = "호롤롤로"
+
+//someValue = Animal()
+//someValue = ("gkgkgk", "ghghgh")
+
+print(type(of: someValue))
